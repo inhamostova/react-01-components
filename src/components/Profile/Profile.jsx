196e76a8dff0d4af287c9b474205a1e8421791d7
inhamostova <inha.mostova@gmail.com>
2026-08-12 12:36:50 +0300

    add first task (Profile component)

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import {
+  CardWrapper,
+  Description,
+  Image,
+  Name,
+  Stats,
+  StatsItem,
+  StatsSpan,
+} from './Profile.styled';
+
+export const Profile = ({
+  user: {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) => {
+  return (
+    <CardWrapper>
+      <Description>
+        <Image src={avatar} alt={tag} />
+        <Name>{username}</Name>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </Description>
+
+      <Stats>
+        <StatsItem>
+          <span>Followers</span>
+          <StatsSpan>{followers}</StatsSpan>
+        </StatsItem>
+        <StatsItem>
+          <span>Views</span>
+          <StatsSpan>{views}</StatsSpan>
+        </StatsItem>
+        <StatsItem>
+          <span>Likes</span>
+          <StatsSpan>{likes}</StatsSpan>
+        </StatsItem>
+      </Stats>
+    </CardWrapper>
+  );
+};
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+
+export const CardWrapper = styled.div`
+  width: 300px;
+  margin: 0 auto;
+  margin-top: 32px;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  background-color: #c9e2c9;
+  border-radius: 10px 10px 0 0;
+
+  p {
+    color: gray;
+    margin: 0;
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+  width: 100px;
+  height: auto;
+  object-fit: cover;
+  background-color: aliceblue;
+  border-radius: 50%;
+`;
+
+export const Name = styled.p`
+  font-weight: 700px;
+  font-size: 24px;
+`;
+
+export const Stats = styled.ul`
+  display: flex;
+  justify-content: center;
+
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  background-color: lightblue;
+  border-radius: 0 0 10px 10px;
+`;
+
+export const StatsItem = styled.li`
+  flex-basis: calc((100%) / 3);
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  padding: 16px;
+  border: 1px solid #000;
+  border-bottom: none;
+
+  :first-of-type {
+    border-left: none;
+  }
+
+  :last-child {
+    border-right: none;
+  }
+`;
+
+export const StatsSpan = styled.span`
+  font-weight: 700;
+  font-size: 20px;
+`;
